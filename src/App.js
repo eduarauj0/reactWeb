61a1f6717687daa5422d4e627d0edff6426ae129
EDUARDO FRANCISCO ARAUJO <s17072@sescpr.com.br>
2023-05-05 14:33:58 -0300

    Signed-off-by: EDUARDO FRANCISCO ARAUJO <s17072@sescpr.com.br>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
 import Car from './teste';
 import Login from './login';
 import Formulario from './formulario';
@@ -15,6 +15,9 @@ import Col from 'react-bootstrap/Col';
 import MultipleSelect from './select';
 import GridMiui2 from './gridMiui2';
 import Data from './data';
+import FormBootstrap from './formBootstrap';
+import Home from './home';
+
 
 
 function App() {
@@ -22,19 +25,60 @@ function App() {
 	<Container fluid="md">
       <Row>
         <Col>
-			 <div className="App">
-			  <h1>Hello World!</h1>
-			  <div><Car brand="Ford" /></div>
-			  <div><Formulario/></div>
-			  <div><Ajax/></div>
-			  <div><Bootstrap/></div>
-			  <div><Popup parametro1="passei 1 parametro"/></div>
-			  <div><Grid /></div>
-			  <div><Grid2 /></div>
-			  <div><GridMiui /></div>
-			  <div><MultipleSelect /></div>
-			  <div><Data /></div>
-			</div>
+			<BrowserRouter>
+				  <div className="App">
+					  <div><Car brand="Ford" /></div>
+					  			  
+					  <nav>
+						  <ul>
+							<li>
+							  <Link to="/formulario">Formulario</Link>
+							</li>
+							<li>
+							  <Link to="/ajax">Ajax</Link>
+							</li>
+							<li>
+							  <Link to="/bootstrap">Bootstrap</Link>
+							</li>
+							<li>
+							  <Link to="/grid">Grid</Link>
+							</li>
+							<li>
+							  <Link to="/grid2">Grid2</Link>
+							</li>
+							<li>
+							  <Link to="/gridMiui">GridMiui</Link>
+							</li>
+							<li>
+							  <Link to="/multipleSelect">MultipleSelect</Link>
+							</li>
+							<li>
+							  <Link to="/data">Data</Link>
+							</li>
+							<li>
+							  <Link to="/formBootstrap">FormBootstrap</Link>
+							</li>
+							<li>
+							  <Link to="/car">Car</Link>
+							</li>
+						  </ul>
+						</nav>
+					  
+					  <Routes>
+						  <Route path="/" element={<Home />} />
+						  <Route path="/formulario" element={<Formulario />} />
+						  <Route path="/ajax" element={<Ajax />} />
+						  <Route path="/bootstrap" element={<Bootstrap />} />
+						  <Route path="/grid" element={<Grid />} />
+						  <Route path="/grid2" element={<Grid2 />} />
+						  <Route path="/gridMiui" element={<GridMiui />} />
+						  <Route path="/multipleSelect" element={<MultipleSelect />} />
+						  <Route path="/data" element={<Data />} />
+						  <Route path="/formBootstrap" element={<FormBootstrap />} />
+						  <Route path="/car" element={<Car brand="Ford" />} />
+					  </Routes>
+				  </div>
+			</BrowserRouter>
 		</Col>
       </Row>
     </Container>
